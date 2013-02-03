4e1125a8d1dec9cbb87034b653e6863436848ca1
Jehiah Czebotar <jehiah@gmail.com>
2013-02-03 01:35:34 +0000

    tackling multi-step jobs

--- runner.go
+++ runner.go
@@ -33,7 +33,6 @@ type Combiner interface {
 }
 
 type Step interface {
-	Mapper
 	Reducer
 }
 
@@ -44,6 +43,7 @@ type Runner struct {
 	Output       string
 	ReducerTasks int
 	tmpPath      string
+	exePath      string
 }
 
 func NewRunner() *Runner {
@@ -68,24 +68,63 @@ func (r *Runner) Cleanup() error {
 	return RMR(r.Output)
 }
 
-func (r *Runner) submitJob(loggerAddress string, processName string, combiner bool) error {
+func (r *Runner) submitJob(loggerAddress string, stepNumber int) error {
+	if stepNumber >= len(r.Steps) || len(r.Steps) == 0 {
+		return fmt.Errorf("step %d out of range", stepNumber)
+	}
+	step := r.Steps[stepNumber]
+	var input []string
+	var output string
+
+	if stepNumber == len(r.Steps)-1 && r.Output != "" {
+		output = r.Output
+	} else {
+		if len(r.Steps) == 1 {
+			output = fmt.Sprintf("%s/output", r.tmpPath)
+		} else {
+			output = fmt.Sprintf("%s/step_%d/output", r.tmpPath, stepNumber)
+		}
+	}
+
+	if stepNumber == 0 {
+		input = r.InputFiles
+	} else {
+		input = append(input, fmt.Sprintf("%s/step_%d/output/part-*", r.tmpPath, stepNumber-1))
+	}
+
+	processName := filepath.Base(r.exePath)
 	remoteLogger := ""
 	if loggerAddress != "" {
 		remoteLogger = fmt.Sprintf(" --remote-logger=%s", loggerAddress)
 	}
 	j := Job{
 		Name:         r.Name,
-		CacheFiles:   []string{fmt.Sprintf("hdfs://%s#%s", processName, filepath.Base(processName))},
+		CacheFiles:   []string{fmt.Sprintf("hdfs://%s#%s", r.exePath, processName)},
 		ReducerTasks: r.ReducerTasks,
-		Input:        r.InputFiles,
-		Output:       r.Output,
-		Mapper:       fmt.Sprintf("%s --stage=mapper%s", filepath.Base(processName), remoteLogger),
-		Reducer:      fmt.Sprintf("%s --stage=reducer%s", filepath.Base(processName), remoteLogger),
+		Input:        input,
+		Output:       output,
+		Reducer:      fmt.Sprintf("%s --stage=reducer%s --step=%d", processName, remoteLogger, stepNumber),
 	}
-	if combiner {
-		j.Combiner = fmt.Sprintf("%s --stage=combiner%s", filepath.Base(processName), remoteLogger)
+	if _, ok := step.(Mapper); ok {
+		j.Mapper = fmt.Sprintf("%s --stage=mapper%s --step=%d", processName, remoteLogger, stepNumber)
+	}
+	if _, ok := step.(Combiner); ok {
+		j.Combiner = fmt.Sprintf("%s --stage=combiner%s --step=%d", processName, remoteLogger, stepNumber)
 	}
 	return SubmitJob(j)
+}
+
+func (r *Runner) copyRunningBinaryToHdfs() error {
+	// copy the current executible binary to hadoop for use as the map reduce tasks
+	r.exePath = fmt.Sprintf("%s/%s", r.tmpPath, "gomrjob_binary")
+	localExePath, err := filepath.EvalSymlinks("/proc/self/exe")
+	if err != nil {
+		return fmt.Errorf("failed locating running executable %s", err)
+	}
+	if err := Put(localExePath, r.exePath); err != nil {
+		return fmt.Errorf("error copying %s to hdfs %s", r.exePath, err)
+	}
+	return nil
 }
 
 func (r *Runner) Run() error {
@@ -106,6 +145,10 @@ func (r *Runner) Run() error {
 
 	switch *stage {
 	case "mapper":
+		s, ok := s.(Mapper)
+		if !ok {
+			return errors.New("step does not support Mapper interface")
+		}
 		if err := s.Mapper(os.Stdin, os.Stdout); err != nil {
 			return err
 		}
@@ -120,7 +163,10 @@ func (r *Runner) Run() error {
 		os.Exit(0)
 		return nil
 	case "combiner":
-		s := s.(Combiner)
+		s, ok := s.(Combiner)
+		if !ok {
+			return errors.New("step does not support Combiner interface")
+		}
 		if err := s.Combiner(os.Stdin, os.Stdout); err != nil {
 			return err
 		}
@@ -129,35 +175,26 @@ func (r *Runner) Run() error {
 		return nil
 	}
 	if !*submitJob {
-		return errors.New("missing --submit-job or --stage")
+		return errors.New("missing --submit-job")
 	}
 
 	log.Printf("submitting map reduce job")
 
 	r.setTempPath()
-	Mkdir(r.tmpPath)
-
-	// copy the current executible binary to hadoop for use as the map reduce tasks
-	exe := fmt.Sprintf("%s/%s", r.tmpPath, "gomrjob_binary")
-	exePath, err := filepath.EvalSymlinks("/proc/self/exe")
-	if err != nil {
-		log.Fatalf("failed stating file")
-	}
-	err = Put(exePath, exe)
-	if err != nil {
-		log.Fatalf("error running Put %s", err)
+	if err := Mkdir(r.tmpPath); err != nil {
+		return err
 	}
 
-	if r.Output == "" {
-		r.Output = fmt.Sprintf("%s/output", r.tmpPath)
+	if err := r.copyRunningBinaryToHdfs(); err != nil {
+		return err
 	}
 
 	loggerAddress := startRemoteLogListner()
-	_, combiner := s.(Combiner)
-	// submit the streaming job
-	err = r.submitJob(loggerAddress, exe, combiner)
-	if err != nil {
-		log.Fatalf("error submitting job %s", err)
+
+	for stepNumber, _ := range r.Steps {
+		if err := r.submitJob(loggerAddress, stepNumber); err != nil {
+			return fmt.Errorf("failed running Step %d= %s", stepNumber, err)
+		}
 	}
 
 	return nil
